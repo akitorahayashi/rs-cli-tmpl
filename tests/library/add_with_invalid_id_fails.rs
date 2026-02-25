dd34bbfd4a259762a1b746a2859c4751bb8e29f4
akitorahayashi <hayashi20030703@gmail.com>
2026-02-25 10:59:01 +0900

    Add test context initialization to invalid ID test case

--- tests/library/add_with_invalid_id_fails.rs
+++ tests/library/add_with_invalid_id_fails.rs
@@ -1,3 +1,4 @@
+use crate::harness::test_context::TestContext;
 use rs_cli_tmpl::add;
 use serial_test::serial;
 use std::io;
@@ -5,6 +6,7 @@ use std::io;
 #[test]
 #[serial]
 fn add_with_invalid_id_fails() {
+    let _ctx = TestContext::new();
     let err = add("invalid/id", "content").expect_err("add should fail for invalid id");
     assert_eq!(err.kind(), io::ErrorKind::InvalidInput);
 }
